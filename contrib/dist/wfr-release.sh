7a13e89a8728395e54011b1e2a6fbbe7ede955f5
Andrew Friedley <andrew.friedley@intel.com>
2014-08-27 11:35:46 -0700

    Add wfr-release.sh script.
    The wfr-release script modifies the Release number in
    contrib/dist/linux/openmpi.spec to the number of commits since the last
    drop release tag.  Resulting RPMs then have the correct release number
    that can be used to determine the corresponding git commit, just like
    other WFR packages/repos.
    
    INTERNAL: PR 125262 - Build version required for openmpi
    
    Use a WFR tag of the form wfr-orig-vX.Y
    INTERNAL: PR 125262 - Build version required for openmpi
    
    Change -d reference in makefile to be -e.
    INTERNAL: PR 129304 - Remove -d .git references in wfr-release.sh
    
    Change-Id: I5ec9d77b704490acd2e1e2b1858d411f2947a0a1
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Andrew Friedley <andrew.friedley@intel.com>
    (cherry picked from commit eb877983cc99cf45113e729bb9a2f66bea277b49)
    Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>
    (cherry picked from commit 4a71c2438c9119feef3b8894d96618cbc6b8d301)
    (cherry picked from commit f797cf8d89a800eb6b604f0865c22f6f19ed8f3b)
    (cherry picked from commit 0d324d96206e05cd6e534b1433780c132a79b6b1)

---
+++ contrib/dist/wfr-release.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Copyright (c) 2014 Intel Corporation. All rights reserved.
+
+# Tags are expected to be of the form wfr-orig-vX.Y{.Z}.  X.Y.Z is the Open MPI
+# version, for example 1.8 or 1.8.2.  Examples of valid full tags:
+# wfr-orig-v1.8 or wfr-orig-v1.8.2
+
+# The release number is calculated as the number of git commits since the last
+# tag of the above form.
+
+TAG="wfr-orig-v"
+RELEASE="release"
+SPEC="contrib/dist/linux/openmpi.spec"
+
+CDUP=`git rev-parse --show-cdup`
+if [ "$CDUP" ]; then
+	echo "CDUP $CDUP"
+	cd $CDUP
+fi
+
+if [ ! -e .git ]; then
+	echo "This script is only to be run inside a WFR Open MPI git tree; aborting."
+	exit 1
+fi
+
+if [ ! -f $SPEC ]; then
+	echo "SPEC file $SPEC not found; aborting.  Is this really a WFR Open MPI git tree?"
+	exit 1
+fi
+
+# git describe has output in the form: wfr-v1.8-9-g840c2f7
+# In that example, this command extracts just the '9'.
+#RELEASE=`git describe --tags --long --match="$TAG*" | sed -r -e "s/$TAG[0-9]+(\.[0-9]+)+-[0-9]+\.[0-9]+-//" -e 's/-g.*$//'`
+RELEASE=`git describe --tags --long --match="$TAG*" | sed -r -e "s/$TAG[0-9]+(\.[0-9]+)+-//" -e 's/-g.*$//'`
+
+echo "WFR commit number: $RELEASE"
+
+sed -i $SPEC -r -e "s/Release: [0-9]+/Release: $RELEASE/"
+
