5b4e3401d77056e78e0b8b3ad399d20855cf9914
Todd Rimmer <todd.rimmer@intel.com>
2015-03-08 19:09:12 -0400

    openmpi: add makesrpm.sh to allow simplification of build processes
    Currently there is a separate import script for each WFR git repro
    Each repro has its own approach to create a source rpm.  These are documented
    on the WFR wiki.  This causes a maintainability issue as it requires 13
    different import scripts each with slightly different algorithms to
    taking a cloned git repro a producing a srpm.  This problem will only get
    worse as it can be anticipate that future revisions may change the process
    and hence require release specific versions of those scripts.
    
    The proposed solution is to have a makesrpm.sh script in each repro which
    implements the algorithm for building the srpm from a fresh git clone/checkout.
    This results in a consistent "API" for use by the import and build to take
    any WFR git repro and create a src.rpm.
    A comparable patch has been submitted and reviewed.  Now moving onto the
    other WFR repos.  As part of this work
    the sandbox build process for creating IFS can also be more automated and
    simplified to take advantage of these scripts in the existing dobuildall script.
    
    INTERNAL: PR 127255 - improve maintainability of git import process
    
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Reviewed-by: John Fleck <john.fleck@intel.com>
    Signed-off-by: Todd Rimmer <todd.rimmer@intel.com>
    Change-Id: I715d41fbb43435f288b23dc8a1df6b14e12ca398
    (cherry picked from commit cc5f53a90cd77282d0d39925ffc25137d3a7ae39)
    Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>
    (cherry picked from commit c2966c0a46b28b6f7866cadb445424bff31e1c0f)
    (cherry picked from commit 31b1086f4008fa74e4a0e16704021172f8a60c48)
    (cherry picked from commit 374993aacb2d571dffffd1d9d21f344b817422f8)

---
+++ makesrpm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RPM_NAME=openmpi
+
+rm -rf BUILD RPMS SOURCES SPECS SRPMS BUILDROOT
+
+./contrib/dist/wfr-release.sh
+./contrib/dist/make_tarball --greekonly --distdir $PWD
+
+mkdir -p ./{BUILD,RPMS,SOURCES,SPECS,SRPMS,BUILDROOT}
+dir=$PWD
+cd contrib/dist/linux
+rpmtopdir=$dir ./buildrpm.sh -R $dir $dir/$RPM_NAME-*.tar.gz
